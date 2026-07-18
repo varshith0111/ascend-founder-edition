fe3b986dd6261a201371a5169f3e6c2e4c8e7dce
Varshith Kumar Raikanti <varshithlucky99@gmail.com>
2026-07-19 00:38:00 +0530

    chore: configure ESLint for shadcn components

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -17,6 +18,13 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+  },
+
+  {
+    files: ['src/components/ui/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
     },
   },
 ])
