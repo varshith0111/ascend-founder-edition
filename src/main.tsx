0de7c67a2bb950886d1b821225ef34794d9486ea
Varshith Kumar Raikanti <varshithlucky99@gmail.com>
2026-07-21 21:33:41 +0530

    feat: establish AppProviders composition root

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AppProviders } from '@/app/AppProviders'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </StrictMode>,
 )
